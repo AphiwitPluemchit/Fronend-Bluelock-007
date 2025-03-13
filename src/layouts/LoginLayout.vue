9f4c6fa0b6dfd15ad0083a63b2d128538f3ea8df
aphiwitrr <168804537+aphiwitrr@users.noreply.github.com>
2025-03-13 22:09:11 +0700

    Auth Login

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <!-- ส่ง event จาก Navbar ไป Sidebar -->
+    <q-page-container style="display: flex">
+      <div class="q-mx-auto"><router-view /></div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts"></script>
