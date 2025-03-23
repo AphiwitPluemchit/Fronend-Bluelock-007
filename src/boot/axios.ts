582c74b11fe63240cd0649cc970f8faad2a21111
aphiwitrr <168804537+aphiwitrr@users.noreply.github.com>
2025-03-23 21:19:51 +0700

    go to dekdee3

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -3,8 +3,8 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: 'http://dekdee3.informatics.buu.ac.th:8888', // เปลี่ยนเป็น URL ของ API หลังบ้าน
-  baseURL: 'http://127.0.0.1:8888',
-  // baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: 'http://127.0.0.1:8888',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000, // กำหนด timeout
   headers: {
     'Content-Type': 'application/json',
