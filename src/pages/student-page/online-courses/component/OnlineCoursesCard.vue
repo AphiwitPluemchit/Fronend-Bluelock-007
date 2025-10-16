d10ab847068d82e7f2167ecbe54abadab6934eff
SahaphapM <65160309@go.buu.ac.th>
2025-10-16 22:14:13 +0700

    refactor: update title computation to use only course name

--- src/pages/student-page/online-courses/component/OnlineCoursesCard.vue
+++ src/pages/student-page/online-courses/component/OnlineCoursesCard.vue
@@ -16,7 +16,7 @@ const props = defineProps<Props>()
 
 // แปลง Course properties เป็นค่าที่ต้องใช้ใน template
 // แสดงชื่อใบประกาศถ้ามี ถ้าไม่มีให้แสดงชื่อหลักสูตรปกติ
-const title = computed(() => props.course.certificateName || props.course.name)
+const title = computed(() => props.course.name)
 const skillType = computed(() => (props.course.isHardSkill ? 'hardSkill' : 'softSkill'))
 const platformType = computed(() => (props.course.type === 'buumooc' ? 'Buu Mooc' : 'Thai Mooc'))
 const hours = computed(() => props.course.hour)
